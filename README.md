a97cd4d5bff508df3b8ee41d778f2a2192089617
Ben Dowling <bendowlingtech@gmail.com>
2024-08-06 19:05:51 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# gavana
+# Gavana
 
 Gavana is a Go web framework. With batteries included this framework allows for a fast and efficient development experience for web development in Go. 
--- graft/graft.go
+++ graft/graft.go
@@ -2,6 +2,7 @@ package graft
 
 import (
 	"github.com/jackc/pgx/v5/pgxpool"
+	"strings"
 )
 
 type Graft struct {
@@ -31,17 +32,33 @@ func (qb QueryBuilder) Select(columns ...string) *QueryBuilder {
 }
 
 func (qb *QueryBuilder) First() error{
-	query := qb.
+	query := qb.buildQuery(true)
+	return qb.graft.db.QueryRow(query, qb.args...).Scan(dest)
 }
 
 func (qb *QueryBuilder) buildQuery(singleRecord bool) string {
 	query := "SELECT "
 
 	if len(qb.selectCols) > 0 {
-
+		query += strings.Join(qb.selectCols, ", ")
 	} else {
+		query += "*"
+	}
+
+	query += " FROM " + qb.table
+
+	if len(qb.whereClauses) > 0 {
 
 	}
+
+	if len(qb.orderByClauses) > 0 {
+		query += " ORDER BY " + strings.Join(qb.orderByClauses, ", ")
+	}
+
+	if singleRecord {
+		query += " LIMIT 1"
+	}
+	return query
 }
 
 
